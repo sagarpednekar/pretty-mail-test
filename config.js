3e4c500d7743cbdbb9ccc5a8d1740f33dfc4a4dc
Sagar Pednekar <sagarpednekar04@gmail.com>
2020-04-18 22:29:13 +0530

    Configure for heroku deployment

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
       password: process.env.REDIS_PASSWORD,
-      url: process.env.REDIS_URI,
+      url: process.env.REDIS_URL,
     },
   },
 };
